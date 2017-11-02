5908d4c01cde478a8ab48471f67eedfcbf9aba51
puzan <puzansakya@gmail.com>
2017-11-02 22:51:03 +0545

    model driven added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  userForm
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      address: new FormGroup({
+        street: new FormControl(),
+        city: new FormControl(),
+        postalCode: new FormControl()
+      })
+    });
+  }
+
+
+
+  onSubmit() {
+    console.log(this.userForm.value);
+    this.userForm.reset();
+  }
+
 }
